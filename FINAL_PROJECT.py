8a7425d2ca8ee571b9cd03b82630f614b3e127a9
Timothy Davis (TDAVI4633) <TDAVI4633@ung.edu>
2018-12-03 19:42:05 -0500

    Added a note
    For this commit I add the error that I got in Arc Pro to a doc string so
    that I have it for nexyt time Added a note
    
    For this commit I add the error that I got in Arc Pro to a doc string so
    that I have it for nexyt time

--- FINAL_PROJECT.py
+++ FINAL_PROJECT.py
@@ -43,9 +43,17 @@ arcpy.Clip_analysis(os.path.join(output_folder, "compbands.tif"), clip_extent, o
 
 #arcpy.Clip_analysis(in_features, clip_features, out_feature_class, xy_tolerance)
 
-""" File "L:\TDAVI4633\4 Fall 2018\GISC 3200K - Python\LandSat_Imagery_Processor\FINAL_PROJECT.py", line 42
-    arcpy.Clip_analysis(os.path.join(output_folder, "compbands.tif")), clip_extent, output_folder, xy_tolerance)
-                                                                                                               ^
-SyntaxError: invalid syntax
+""" Traceback (most recent call last):
+  File "L:\TDAVI4633\4 Fall 2018\GISC 3200K - Python\LandSat_Imagery_Processor\FINAL_PROJECT.py", line 42, in <module>
+    arcpy.Clip_analysis(os.path.join(output_folder, "compbands.tif"), clip_extent, os.path.join(output_folder, "clip_composit.tif"), xy_tolerance)
+  File "c:\program files\arcgis\pro\Resources\arcpy\arcpy\analysis.py", line 63, in Clip
+    raise e
+  File "c:\program files\arcgis\pro\Resources\arcpy\arcpy\analysis.py", line 60, in Clip
+    retval = convertArcObjectToPythonObject(gp.Clip_analysis(*gp_fixargs((in_features, clip_features, out_feature_class, cluster_tolerance), True)))
+  File "c:\program files\arcgis\pro\Resources\arcpy\arcpy\geoprocessing\_base.py", line 506, in <lambda>
+    return lambda *args: val(*gp_fixargs(args, True))
+arcgisscripting.ExecuteError: Failed to execute. Parameters are not valid.
+ ERROR 000732: Input Features: Dataset L:\TDAVI4633\4 Fall 2018\GISC 3200K - Python\Python\compbands.tif does not exist or is not supported
+Failed to execute (Clip).s
  Failed to execute (Unzip).
 """
